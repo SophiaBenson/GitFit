06d12c2fa7ead32535f629fa9af384c69760196b
Sophia Benson <benso898@umn.edu>
2016-07-07 13:37:40 -0500

    using Auth0

--- modules/connection.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-// 27017 is default mongo port
-mongoose.connect('localhost:/27017/soloProject');
-
-module.exports = mongoose;
--- server/app.js
+++ server/app.js
@@ -1,32 +1,59 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var passport = require('../strategies/user-local.js');
 var session = require('express-session');
+var passport = require('passport');
+// This is the file we created in step 2.
+// This will configure Passport to use Auth0
+var strategy = require('../setup-passport');
 
+// Session and cookies middlewares to keep user logged in
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+app.use(cookieParser());
+// See express session docs for information on the options: https://github.com/expressjs/session
+app.use(session({ secret: 'c2QOfPozJgj4JYNhLYWQD0sdaoAVpfBlya4EP5cjJik5tiYaaPhhWg7bPxO2zILj', resave: false,  saveUninitialized: false }));
+
+// Auth0 callback handler
+app.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.redirect("/user");
+  });
+
+  app.get('/user', function (req, res) {
+    res.render('user', {
+      user: req.user
+    });
+  });
+
+//End of Auth0 google account authentication.
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(session({
-  secret: 'any string',
-  key: 'user',
-  resave: 'true',
-  saveUninitialized: false,
-  cookie: {maxage: 60000, secure: false}
-}));//end app.use
+// app.use(session({
+//   secret: 'any string',
+//   key: 'user',
+//   resave: 'true',
+//   saveUninitialized: false,
+//   cookie: {maxage: 60000, secure: false}
+// }));//end app.use
 
 //passport
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 // require routers
 var index = require('../routes/index');
-var register = require('../routes/register');
 
 app.use(express.static('public'));
 
 //register and login routes
-app.use('/register', register);
+
 app.use('/*', index);
 
 //spin up server
