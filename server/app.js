535f6fb7c0ef2ce93ecdd02da6dc24afbc4665cf
Sophia Benson <benso898@umn.edu>
2016-07-07 16:49:02 -0500

    authentication redirect

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
+var path = require('path');
 // This is the file we created in step 2.
 // This will configure Passport to use Auth0
 var strategy = require('../setup-passport');
@@ -27,7 +28,9 @@ app.get('/callback',
   app.get('/user', function (req, res) {
     res.render('user', {
       user: req.user
+
     });
+    
   });
 
 //End of Auth0 google account authentication.
@@ -49,13 +52,16 @@ app.use(passport.session());
 
 // require routers
 var index = require('../routes/index');
-
+// var main = require('../public/views/main.html');
 app.use(express.static('public'));
 
 //register and login routes
 
-app.use('/*', index);
-
+app.use('/', index);
+app.get('/main', function (req, res) {
+  console.log('In main schedule page');
+  res.sendFile(path.resolve('public/views/main.html'));
+});
 //spin up server
 app.listen(3000, function () {
   console.log("server is listening on port 3000");
